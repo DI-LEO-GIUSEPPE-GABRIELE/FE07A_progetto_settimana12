f85be9f8fc59550802b3b03e18303cf32c5d9e0a
Jimbo9526 <giuseppegabrieledileo95@gmail.com>
2022-03-04 18:04:13 +0100

    migliorie grafiche

--- src/app/features/movies/movies.page.ts
+++ src/app/features/movies/movies.page.ts
@@ -5,25 +5,25 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   template: `
-    <div *ngFor="let movie of movies" class="container text-center">
-      <div class="row justify-content-center">
-        <div style="width: 18rem;">
+    <div class="container text-center">
+      <div class="row justify-content-between">
+        <div *ngFor="let movie of movies" class="card mt-5 p-3 bg-dark text-white rounded" style="width: 18rem; margin: 0 auto;">
           <img
-            src=""
-            alt=""
-            class="img-fluid"
+            class="img-fluid rounded"
             srcset="http://image.tmdb.org/t/p/w500{{movie.poster_path}}"
           />
           <div class="card-body">
-            <h5 class="card-title">{{ movie.title }}</h5>
-            <p class="card-text">{{ movie.overview }}</p>
-            <p>{{ movie.popularity }}</p>
-            <p>{{ movie.release_date }}</p>
-            <button *ngIf="movie.like" (click)="unlike(movie)">
-              <svg
+            <h4 class="card-title">{{ movie.title }}</h4>
+           <div class="card-text">
+              <p><a class="text-danger" [routerLink]="['movies-details']">Scopri la trama...</a></p>
+              <!-- <p class="card-text">{{ movie.overview }}</p> -->
+              <p>{{ movie.popularity }}</p>
+              <p>{{ movie.release_date }}</p>
+           </div>
+              <svg *ngIf="movie.like" (click)="unlike(movie)"
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width="30"
+                height="30"
                 fill="red"
                 class="bi bi-heart-fill"
                 viewBox="0 0 16 16"
@@ -33,12 +33,10 @@ import { HttpClient } from '@angular/common/http';
                   d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                 />
               </svg>
-            </button>
-            <button *ngIf="!movie.like" (click)="like(movie)">
-              <svg
+              <svg *ngIf="!movie.like" (click)="like(movie)"
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width="30"
+                height="30"
                 fill="currentColor"
                 class="bi bi-heart"
                 viewBox="0 0 16 16"
@@ -47,13 +45,20 @@ import { HttpClient } from '@angular/common/http';
                   d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
                 />
               </svg>
-            </button>
           </div>
         </div>
       </div>
     </div>
   `,
-  styles: [],
+  styles: [`
+    svg {
+      cursor:pointer;
+    }
+
+    svg:hover {
+      transform: scale(1.1)
+    }
+  `],
 })
 export class MoviesPage implements OnInit {
   constructor(private movieSrv: MovieService, private http: HttpClient) {}
